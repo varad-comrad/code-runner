5cd21316fa638fa42cde423afd7d52a8036192b5
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-14 08:55:35 -0300

    created python and rust versions

---
+++ coderunner-rust/target/debug/deps/coderunner_rust-db52db7aef7c1388.d
@@ -0,0 +1,5 @@
+/home/fabricio/programs/test/code-runner/code-runner/coderunner-rust/target/debug/deps/coderunner_rust-db52db7aef7c1388.rmeta: src/main.rs
+
+/home/fabricio/programs/test/code-runner/code-runner/coderunner-rust/target/debug/deps/coderunner_rust-db52db7aef7c1388.d: src/main.rs
+
+src/main.rs:
---
+++ coderunner-rust/target/debug/deps/coderunner_rust-e26ecb25b9f6033f.d
@@ -0,0 +1,5 @@
+/home/fabricio/programs/test/code-runner/code-runner/coderunner-rust/target/debug/deps/coderunner_rust-e26ecb25b9f6033f.rmeta: src/main.rs
+
+/home/fabricio/programs/test/code-runner/code-runner/coderunner-rust/target/debug/deps/coderunner_rust-e26ecb25b9f6033f.d: src/main.rs
+
+src/main.rs:
